044df63d1260e996770e3e530f97732c0c70a093
grol <getahune.alemayhu@gmail.com>
2025-12-24 16:22:07 +0300

    feat: implement initial JavaScript functionality for difficulty and mode selection

---
+++ script.js
@@ -0,0 +1,60 @@
+const easybutton = document.querySelector(".easy-button");
+const mediumbutton = document.querySelector(".medium-button");
+const hardbutton = document.querySelector(".hard-button");
+
+const timemodebutton = document.querySelector(".Time-mode-button");
+const passagemodebutton = document.querySelector(".passage-mode-button");
+
+const start = document.querySelector(".start-button");
+const reset = document.querySelector(".reset-button");
+
+const textarea = document.querySelector(".text-input");
+const teststate = document.querySelectorAll(".test-state");
+
+const difficultySettings = document.querySelectorAll(
+  ".difficulty-settings button"
+);
+
+const modeSettings = document.querySelectorAll(".mode-settings button");
+
+difficultySettings.forEach((button) => {
+  button.addEventListener("click", () => {
+    // remove active from all
+    difficultySettings.forEach((d) => d.classList.remove("active"));
+    // add active to the clicked one
+    button.classList.add("active");
+  });
+});
+
+modeSettings.forEach((buttons) => {
+  buttons.addEventListener("click", () => {
+    // remove active from all
+    modeSettings.forEach((s) => s.classList.remove("active"));
+    // add active to the clicked one
+    buttons.classList.add("active");
+  });
+});
+
+//Add click listener to start button → call a startTest() function
+
+function startTest() {
+  console.log("statred");
+}
+start.addEventListener("click", () => {
+  if (start) {
+    startTest();
+  }
+});
+
+// const timemodebutton = document
+// function showState(stateName) {
+//   document.querySelectorAll('.test-state').forEach(state => {
+//     state.classList.remove('active');
+//   });
+//   document.getElementById(stateName).classList.add('active');
+// }
+
+// // Usage:
+// showState('test-active');    // Show typing screen
+// showState('test-results');   // Show results
+// showState('test-setup');     // Back to initial
